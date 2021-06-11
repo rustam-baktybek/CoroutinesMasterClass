89e238d6fd79e25c393a718f37150bf8c19c49e0
Rustam Bakytov <rustam.baktybek@pricer.kg>
2021-06-11 16:04:53 +0600

    Add empty space

--- app/src/main/java/com/example/corotinesmasterclass/MainActivity.kt
+++ app/src/main/java/com/example/corotinesmasterclass/MainActivity.kt
@@ -16,6 +16,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         CoroutineScope(Dispatchers.Default).launch {
             Log.d("--------","coroutines")
+
+
+
         }
     }
 }
