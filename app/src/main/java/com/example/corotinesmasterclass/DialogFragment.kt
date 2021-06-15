0bd34e842648d605c6e077c886e58bfd2d2cd124
Rustam Bakytov <rustam.baktybek@pricer.kg>
2021-06-15 09:34:28 +0600

    DialogFragment.kt: Add empty space

--- app/src/main/java/com/example/corotinesmasterclass/DialogFragment.kt
+++ app/src/main/java/com/example/corotinesmasterclass/DialogFragment.kt
@@ -15,6 +15,9 @@ class DialogFragment : BottomSheetDialogFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+
+
     }
 
 }
