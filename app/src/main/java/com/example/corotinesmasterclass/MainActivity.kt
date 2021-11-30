969f5644f3cabc5139d4ee25d816cb91ab973fa4
Rustam Bakytov <rustam.baktybek@pricer.kg>
2021-11-30 14:37:19 +0600

    MainActivity: Clear code

--- app/src/main/java/com/example/corotinesmasterclass/MainActivity.kt
+++ app/src/main/java/com/example/corotinesmasterclass/MainActivity.kt
@@ -2,57 +2,13 @@ package com.example.corotinesmasterclass
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.work.PeriodicWorkRequestBuilder
-import androidx.work.WorkManager
-import java.util.concurrent.TimeUnit
-import java.util.concurrent.atomic.AtomicInteger
 
 
 class MainActivity : AppCompatActivity() {
-    val sharedCounter: AtomicInteger = AtomicInteger(0)
-
-
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val myWorkRequest5 =
-            PeriodicWorkRequestBuilder<MyWorker>(
-                15,
-                TimeUnit.DAYS,
-                17,
-                TimeUnit.MINUTES
-            )
-                .build()
-
-        WorkManager.getInstance(this).enqueue(myWorkRequest5)
-
-
-
-
-        val myWorkRequest1=
-            PeriodicWorkRequestBuilder<MyWorker>(
-                15,
-                TimeUnit.DAYS,
-                17,
-                TimeUnit.MINUTES
-            )
-                .build()
-
-        WorkManager.getInstance(this).enqueue(myWorkRequest1)
-
-        val myWorkRequest2 =
-            PeriodicWorkRequestBuilder<MyWorker>(
-                15,
-                TimeUnit.DAYS,
-                17,
-                TimeUnit.MINUTES
-            )
-                .build()
-
-        WorkManager.getInstance(this).enqueue(myWorkRequest2)
     }
 }
 
